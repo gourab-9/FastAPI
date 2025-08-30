65776e312ff49f70c08916d0a95b831eef71cf72
Gourab Kumar <114401540+gourab-9@users.noreply.github.com>
2025-08-30 15:43:33 +0530

    Update 01_basic_app.py

--- Building_api/01_basic_app.py
+++ Building_api/01_basic_app.py
@@ -7,11 +7,3 @@ app = FastAPI()
 def home():
     return {'message': 'Hello welcome to my FastAPI!'}
 
-# 01--- basic app
-# 02--- pydantic demo
-# 03--- models
-# 04--- main
-# 05--- model-val
-# 06--- sync demo
-# 07--- async demo
-# 08--- async main
